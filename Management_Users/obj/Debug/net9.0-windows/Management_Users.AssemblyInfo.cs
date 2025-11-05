b256801c3cc63385d3eb4722e644da8af5344de7
lucascampos04 <camposdlucasoli@gmail.com>
2025-11-05 09:17:12 -0300

    Apagandoi tudo

--- Management_Users/obj/Debug/net9.0-windows/Management_Users.AssemblyInfo.cs
+++ Management_Users/obj/Debug/net9.0-windows/Management_Users.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Management_Users")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2fb80cc0f3e3b14cd6ab17328d85dd448ae2c95")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+152014d00742c0d8a839b6889335eaad11235143")]
 [assembly: System.Reflection.AssemblyProductAttribute("Management_Users")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Management_Users")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
